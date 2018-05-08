9a82fe0d0db098e2ce2c47d9e4680e4e9b37a32a
zhongweili2010 <38505557+zhongweili2010@users.noreply.github.com>
2018-05-07 23:56:20 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -7,23 +7,40 @@ Created on Thu May  3 09:23:38 2018
 
 import requests
 import json
+import pprint
+import pandas as pd
 
-class stock_data():
-    def __init__(self):
-        self.function="TIME_SERIES_INTRADAY"
-        self.symbol="MSFT"
-        self.interval="1min"
-        self.outputsize = "compact"
-        self.datatype="jason"
-        self.apikey="IQQ6W4ESX1DDHQNB"
-     
-     
-     
+class api:
+    def __init__(self,function="TIME_SERIES_INTRADAY",symbol="MSFT",interval="1min",outputsize="compact",datatype="jason",apikey="IQQ6W4ESX1DDHQNB"):
+        self.function=function
+        self.symbol=symbol
+        self.interval=interval
+        self.outputsize=outputsize
+        self.datatype=datatype
+        self.apikey=apikey
 
 
-connection =  requests.get("https://api.ethplorer.io/getTokenInfo/0xff71cb760666ab06aa73f34995b42dd4b85ea07b?apiKey=freekey")
-print(connection.status_code)
+    def get_function(self):
+        return self.function
+    def get_interval(self):
+        return self.interval
+    def get_url(self):
+        return "https://www.alphavantage.co/query?function=" + self.function\
+                   + "&symbol=" + self.symbol\
+                   + "&interval=" + self.interval\
+                   + "&outputsize=" + self.outputsize\
+                   + "&apikey=" + self.apikey
+     
+
+handle_to_api = api()
+
+connection =  requests.get(handle_to_api.get_url())
+
+print("status code: " + str(connection.status_code))
 #print(type(connection.content))
 content = json.loads(connection.content.decode())
-print(content["address"])
-
+#print(type(content))
+#pprint.PrettyPrinter().pprint(content["Meta Data"])
+data = pd.DataFrame(content)
+#data = pd.DataFrame.from_dict(content["Time Series (" + handle_to_api.get_interval()+")"], orient='row')
+print(data)
